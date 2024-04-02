c038ee732c3fd31cbdc304aedb901af88d0e7c0c
Nanfack95 <125035300+Nanfack95@users.noreply.github.com>
2024-04-02 09:48:02 -0700

    Authentification page connection !!!

--- app/src/main/java/com/example/chatapp/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/example/chatapp/activities/SplashScreenActivity.kt
@@ -1,12 +1,24 @@
 package com.example.chatapp.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.example.chatapp.R
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+    Handler(Looper.getMainLooper()).postDelayed({
+        Intent(this, AuthentificationActivity::class.java).also {
+            startActivity(it)
+        }
+        finish()
+    }, 3000)
+
+
     }
 }
